53d964e03708729b23a3f09178f9a66be952638e
Murutu <murutupeter@email.com>
2020-02-27 17:30:45 +0300

    made changes

--- news/views.py
+++ news/views.py
@@ -75,7 +75,7 @@ def past_days_news(request,past_date):
 
 def news_today(request):
     date = dt.date.today()
-    news = Article.today_news()
+    news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 def search_results(request):
